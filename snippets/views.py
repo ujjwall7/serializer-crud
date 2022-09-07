42a782ac3c220242f458f7cae0f50c05f790e2e7
ujjwal <sharmaujjwal0921@gmail.com>
2022-09-07 09:31:44 +0530

    intial commit

---
+++ snippets/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student
+from .serializers import StudentSerializer
+
+
+
+class StudentAPI(APIView):
+    def get(self, request, pk=None, format=None):
+        id=pk
+        if id is not None:
+            stu=Student.objects.get(id=id)
+            serializer=StudentSerializer(stu)
+            return Response(serializer.data)
+
+        stu = Student.objects.all() 
+        serializer = StudentSerializer(stu, many=True) 
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Date Created'}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+
+    def put(self,request,pk,format=None):
+        id = pk 
+        stu = Student.objects.get(pk=id) 
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save() 
+            return Response({ 'msg' : 'Complete Data Updated'}) 
+        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def patch(self,request,pk,format= None):
+        id = pk 
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save() 
+            return Response({ 'msg' : 'Partial Data Updated'}) 
+        return Response (serializer.errors)
+
+
+    def delete(self, request, pk, format=None):
+        id = pk 
+        stu = Student.objects.get(pk=id) 
+        stu.delete() 
+        return Response({'msg' : 'Data Deleted' })
+
+
+    
